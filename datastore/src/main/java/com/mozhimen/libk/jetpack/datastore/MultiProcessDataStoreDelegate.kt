3194c6f34db1e87ef915988883217889d63cb3db
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-06-14 23:58:48 +0800

    0614

---
+++ datastore/src/main/java/com/mozhimen/libk/jetpack/datastore/MultiProcessDataStoreDelegate.kt
@@ -0,0 +1,95 @@
+package com.mozhimen.libk.jetpack.datastore
+
+import android.content.Context
+import androidx.annotation.GuardedBy
+import androidx.datastore.core.DataMigration
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.MultiProcessDataStoreFactory
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.core.okio.OkioStorage
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.PreferencesSerializer
+import androidx.datastore.preferences.preferencesDataStoreFile
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import okio.FileSystem
+import okio.Path.Companion.toOkioPath
+import java.io.File
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+/**
+ * @ClassName MultiProcessDataStoreDelegate
+ * @Description TODO
+ * @Author Mozhimen & Kolin Zhao
+ * @Date 2024/6/14
+ * @Version 1.0
+ */
+public fun multiProcessPreferencesDataStore(
+    name: String,
+    corruptionHandler: ReplaceFileCorruptionHandler<Preferences>? = null,
+    produceMigrations: (Context) -> List<DataMigration<Preferences>> = { listOf() },
+    scope: CoroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+): ReadOnlyProperty<Context, DataStore<Preferences>> {
+    return MultiProcessPreferenceDataStoreSingletonDelegate(name, corruptionHandler, produceMigrations, scope)
+}
+
+internal class MultiProcessPreferenceDataStoreSingletonDelegate internal constructor(
+    private val name: String,
+    private val corruptionHandler: ReplaceFileCorruptionHandler<Preferences>?,
+    private val produceMigrations: (Context) -> List<DataMigration<Preferences>>,
+    private val scope: CoroutineScope
+) : ReadOnlyProperty<Context, DataStore<Preferences>> {
+
+    private val lock = Any()
+
+    @GuardedBy("lock")
+    @Volatile
+    private var INSTANCE: DataStore<Preferences>? = null
+
+    /**
+     * Gets the instance of the DataStore.
+     *
+     * @param thisRef must be an instance of [Context]
+     * @param property not used
+     */
+    override fun getValue(thisRef: Context, property: KProperty<*>): DataStore<Preferences> {
+        return INSTANCE ?: synchronized(lock) {
+            if (INSTANCE == null) {
+                val applicationContext = thisRef.applicationContext
+
+                INSTANCE = create(
+                    corruptionHandler = corruptionHandler,
+                    migrations = produceMigrations(applicationContext),
+                    scope = scope
+                ) {
+                    applicationContext.preferencesDataStoreFile(name)
+                }
+            }
+            INSTANCE!!
+        }
+    }
+
+    private fun create(
+        corruptionHandler: ReplaceFileCorruptionHandler<Preferences>? = null,
+        migrations: List<DataMigration<Preferences>> = listOf(),
+        scope: CoroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
+        produceFile: () -> File
+    ): DataStore<Preferences> {
+        val delegate = MultiProcessDataStoreFactory.create(
+            storage = OkioStorage(FileSystem.SYSTEM, PreferencesSerializer) {
+                val file = produceFile()
+                check(file.extension == "preferences_pb") {
+                    "File extension for file: $file does not match required extension for" +
+                            " Preferences file: ${"preferences_pb"}"
+                }
+                file.absoluteFile.toOkioPath()
+            },
+            corruptionHandler = corruptionHandler,
+            migrations = migrations,
+            scope = scope
+        )
+        return delegate
+    }
+}
